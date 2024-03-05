336805ce16eb8d762e39adf97f2af431a341692b
nandini <jitcse.ndeshpande2001@gmail.com>
2024-03-05 10:28:43 +0530

    This is the 3 commit

---
+++ FirstProgram.js
@@ -0,0 +1,3 @@
+console.log("This is the first program of the TypeScript");
+var val1 = 70;
+console.log(val1);
--- FirstProgram.ts
+++ FirstProgram.ts
@@ -1,1 +1,7 @@
+//npm install -g typescript
+//
 console.log("This is the first program of the TypeScript");
+
+const  val1 : number=70;
+
+console.log(val1);
