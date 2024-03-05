3c3b63335a3c7b20e3669653d3112a192a6dec27
nandini <jitcse.ndeshpande2001@gmail.com>
2024-03-05 10:58:33 +0530

    6 commit

--- FirstProgram.js
+++ FirstProgram.js
@@ -1,3 +1,14 @@
 console.log("This is the first program of the TypeScript");
 var val1 = 70;
 console.log(val1);
+
+
+//git batch
+//for push and commit the files on the git 
+
+// $ git status   => for knowing the status
+// $ git add .   => for selecting the all file 
+// $ git push
+// $ git status
+// $ git commit -m "commit msg" 
+// then updated file added successfully 
